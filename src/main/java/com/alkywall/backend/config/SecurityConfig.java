03f289dd59323fecb5aac9fa22c315eaf82e96b6
Gastón Alvarez <gaston_a96@hotmail.com>
2026-08-27 10:43:45 -0300

    feat: Creación del SecurityConfig

--- src/main/java/com/alkywall/backend/config/SecurityConfig.java
+++ src/main/java/com/alkywall/backend/config/SecurityConfig.java
@@ -1,10 +1,32 @@
 package com.alkywall.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 }
