3676de34ea541a12b49e77849cc1554a47cb2c2a
Gastón Alvarez <gaston_a96@hotmail.com>
2026-08-29 20:53:52 -0300

    feat: Implementa endpoint en CuentaController

---
+++ backend/src/main/java/com/alkywall/backend/controllers/CuentaController.java
@@ -0,0 +1,30 @@
+package com.alkywall.backend.controllers;
+
+import com.alkywall.backend.dtos.CuentaDTO;
+import com.alkywall.backend.services.ICuentaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cuentas")
+public class CuentaController {
+
+    private final ICuentaService cuentaService;
+
+    public CuentaController(ICuentaService cuentaService) {
+        this.cuentaService = cuentaService;
+    }
+
+    @GetMapping("/balance")
+    public ResponseEntity<CuentaDTO> obtenerBalance(Authentication authentication) {
+
+        String email = authentication.getName();
+
+        CuentaDTO cuentaDTO = cuentaService.obtenerBalancePorEmail(email);
+
+        return ResponseEntity.ok(cuentaDTO);
+    }
+}
--- backend/src/main/java/com/alkywall/backend/repositories/CuentaRepository.java
+++ backend/src/main/java/com/alkywall/backend/repositories/CuentaRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
     Optional<Cuenta> findByUsuarioEmail(String email);
 
-    Optional<Cuenta> findByIdUsuario(Long id);
+    Optional<Cuenta> findByUsuario_IdUsuario(Long id);
 }
