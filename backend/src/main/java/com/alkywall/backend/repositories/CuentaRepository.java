8c9922d2115f88aeecf44a4312157059a1c81037
Gastón Alvarez <gaston_a96@hotmail.com>
2026-08-29 19:40:38 -0300

    feat: Agrega método a CuentaService y CuentaRepository

--- backend/src/main/java/com/alkywall/backend/repositories/CuentaRepository.java
+++ backend/src/main/java/com/alkywall/backend/repositories/CuentaRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 @Repository
 public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
     Optional<Cuenta> findByUsuarioEmail(String email);
+
+    Optional<Cuenta> findByIdUsuario(Long id);
 }
