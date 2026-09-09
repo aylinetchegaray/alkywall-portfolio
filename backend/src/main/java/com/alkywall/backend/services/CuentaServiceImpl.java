e31980a4114b2342d05e29103b07054955e78d04
Gastón Alvarez <gaston_a96@hotmail.com>
2026-09-09 01:17:46 -0300

    feature: Agrega conversión de divisas para el balance

--- backend/src/main/java/com/alkywall/backend/services/CuentaServiceImpl.java
+++ backend/src/main/java/com/alkywall/backend/services/CuentaServiceImpl.java
@@ -11,9 +11,11 @@ import org.springframework.stereotype.Service;
 public class CuentaServiceImpl implements ICuentaService {
 
     private final CuentaRepository cuentaRepository;
+    private final DolarService dolarService;
 
-    public CuentaServiceImpl(CuentaRepository cuentaRepository) {
+    public CuentaServiceImpl(CuentaRepository cuentaRepository, DolarService dolarService) {
         this.cuentaRepository = cuentaRepository;
+        this.dolarService = dolarService;
     }
 
     @Override
@@ -41,6 +43,9 @@ public class CuentaServiceImpl implements ICuentaService {
         dto.setAlias(cuenta.getAlias());
         dto.setId_usuario(id);
         dto.setCuentaId(cuenta.getIdCuenta());
+
+        dto.setCotizacionDolar(dolarService.obtenerCotizacion());
+        dto.setFechaCotizacion(dolarService.obtenerFechaCotizacion());
 
         return dto;
     }
