1966e43c03071f80135f16d7fd8d69ea9ea488e5
Agustin-Collueque <agustincv1997@gmail.com>
2026-09-10 14:57:15 -0300

    Se agrego una validacion de control para los erroes 404 al momento de transferir a un alias o cbu.

--- frontend/transferencias/transferencia.js
+++ frontend/transferencias/transferencia.js
@@ -136,9 +136,22 @@ form.addEventListener('submit', async function (evento) {
             })
         });
 
+<<<<<<< Updated upstream
         if (!respuesta.ok) {
             const textoError = await extraerMensajeError(respuesta);
             console.log(textoError)
+=======
+        if (respuesta.status === 404) {
+            if (alias) {
+                mensaje.textContent = "No se encontró el ALIAS";
+            } else if(cbu) {
+                mensaje.textContent = "No se encontró el CBU";
+            }
+            mensaje.className = 'transferencia-mensaje transferencia-error-general';
+            return;
+        } else if (!respuesta.ok) {
+            const textoError = extraerMensajeError(respuesta);
+>>>>>>> Stashed changes
             mensaje.textContent = textoError;
             mensaje.className = 'transferencia-mensaje transferencia-error-general';
             return;
